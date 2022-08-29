2fb84396e0dfed290bff45fc6865e5fa2bc1ef48
Derek Lewis <DerekNonGeneric@inf.is>
2022-08-28 23:05:03 -0400

    Update .eslintrc.cjs
    Signed-off-by: Derek Lewis <DerekNonGeneric@inf.is>

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -33,7 +33,7 @@ module.exports = {
         'plugin:@typescript-eslint/recommended',
         'plugin:@typescript-eslint/recommended-requiring-type-checking',
         'plugin:@typescript-eslint/strict',
-        'plugin:prettier/recommended', // // Disables rules conflicting w/ Prettier.
+        'plugin:prettier/recommended', // Disables rules conflicting w/ Prettier.
       ],
     },
     {
