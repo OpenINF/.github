6984f8692feb7ed30f95e5391dcdb9c6a54e164e
Derek Lewis <dereknongeneric@open.inf.is>
2023-04-07 03:00:03 +0000

    build: make visibility corrections to task output
    Signed-off-by: Derek Lewis <dereknongeneric@open.inf.is>

--- build/tasks/util.mjs
+++ build/tasks/util.mjs
@@ -1,13 +1,35 @@
 import text from '@openinf/util-text';
-import { fileURLToPath as pathFromFileUrl } from 'url';
-import { echo, os, path } from 'zx';
+import url from 'url';
+import { chalk, echo, os, path } from 'zx';
 
-const __filename = pathFromFileUrl(import.meta.url);
+const __filename = url.fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
+
+/**
+ * Helper to make it possible to use absolute project paths.
+ * @param  {string} relativePath A path relative to the project root
+ * @return {string} An absolute ready-to-use path
+ */
+function absolute(relativePath) {
+  return path.resolve(path.dirname(path.dirname(__dirname)), relativePath);
+}
+
+/**
+ * Various paths that are critical to the project and used throughout.
+ */
+export const PATHS = {
+  projectRoot: absolute('.'),
+  manifest: absolute('./package.json'),
+};
+
+function taskURLToRelativePath(taskURL) {
+  const taskPath = url.fileURLToPath(taskURL);
+  return path.relative(PATHS.projectRoot, taskPath);
+}
 
 export function echoTaskRunning(taskName, taskURL) {
   echo(
-    text.blueify(
+    chalk.cyan(
       `${
         os.EOL +
         String(text.UnicodeEscapes.midlineEllipsis)
@@ -18,26 +40,9 @@ export function echoTaskRunning(taskName, taskURL) {
         String(text.UnicodeEscapes.midlineEllipsis)
           .padStart(3, ' ')
           .padEnd(6, ' ') +
-        text.curlyQuote(taskURL) +
+        text.curlyQuote(taskURLToRelativePath(taskURL)) +
         os.EOL
       }`
     )
   );
 }
-
-/**
- * Helper to make it possible to use absolute project paths.
- * @param  {string} relativePath A path relative to the project root
- * @return {string} An absolute ready-to-use path
- */
-function absolute(relativePath) {
-  return path.resolve(path.dirname(__dirname), relativePath);
-}
-
-/**
- * Various paths that are critical to the project and used throughout.
- */
-export const PATHS = {
-  projectRoot: absolute('.'),
-  manifest: absolute('./package.json'),
-};
