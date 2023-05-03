cf4455f91f926dcd3cfac244a721f6d4b69081ae
Derek Lewis <dereknongeneric@open.inf.is>
2023-05-02 20:52:40 -0400

    build: re-enable format/verify-toml task w/ dprint

--- build/tasks/format/format-toml.mjs
+++ build/tasks/format/format-toml.mjs
@@ -1,30 +1,24 @@
-// import { execute } from '@yarnpkg/shell';
-// import { $ } from 'zx';
+import { execute } from '@yarnpkg/shell';
+import { $ } from 'zx';
 
 import { echoTaskRunning } from '../util.mjs';
 
 echoTaskRunning('format-toml', import.meta.url);
 
-// const TOMLObject =
-//   await $`bundle exec github-linguist --breakdown --json | jq '.TOML.files'`;
-// const TOMLFiles = JSON.parse(TOMLObject.stdout);
+const TOMLObject =
+  await $`bundle exec github-linguist --breakdown --json | jq '.TOML.files'`;
+const TOMLFiles = JSON.parse(TOMLObject.stdout);
 
-// let exitCode = 0;
-// const scripts = [
-//   `eslint --fix ${TOMLFiles.join(' ')}`, // validate & style-check
-// ];
+let exitCode = 0;
+const scripts = [
+  `dprint fmt ${TOMLFiles.join(' ')}`, // validate & style-check
+];
 
-// for await (const element of scripts) {
-//   try {
-//     exitCode = await execute(`pnpm exec ${element}`);
-//   } catch (p) {
-//     exitCode = p.exitCode;
-//   }
-//   process.exitCode = exitCode > 0 ? exitCode : 0;
-// }
-
-// TODO(DerekNonGeneric): Implement this task.
-process.exitCode = 0;
-
-// eslint-disable-next-line unicorn/no-process-exit
-process.exit(0);
+for await (const element of scripts) {
+  try {
+    exitCode = await execute(`pnpm exec ${element}`);
+  } catch (p) {
+    exitCode = p.exitCode;
+  }
+  process.exitCode = exitCode > 0 ? exitCode : 0;
+}
--- build/tasks/verify/verify-toml.mjs
+++ build/tasks/verify/verify-toml.mjs
@@ -1,27 +1,24 @@
-// import { execute } from '@yarnpkg/shell';
-// import { $ } from 'zx';
+import { execute } from '@yarnpkg/shell';
+import { $ } from 'zx';
 
 import { echoTaskRunning } from '../util.mjs';
 
 echoTaskRunning('verify-toml', import.meta.url);
 
-// const TOMLObject =
-//   await $`bundle exec github-linguist --breakdown --json | jq '.TOML.files'`;
-// const TOMLFiles = JSON.parse(TOMLObject.stdout);
+const TOMLObject =
+  await $`bundle exec github-linguist --breakdown --json | jq '.TOML.files'`;
+const TOMLFiles = JSON.parse(TOMLObject.stdout);
 
-// let exitCode = 0;
-// const scripts = [
-//   `eslint ${TOMLFiles.join(' ')}`, // validate & style-check
-// ];
+let exitCode = 0;
+const scripts = [
+  `dprint check ${TOMLFiles.join(' ')}`, // validate & style-check
+];
 
-// for await (const element of scripts) {
-//   try {
-//     exitCode = await execute(`pnpm exec ${element}`);
-//   } catch (p) {
-//     exitCode = p.exitCode;
-//   }
-//   process.exitCode = exitCode > 0 ? exitCode : 0;
-// }
-
-// TODO(DerekNonGeneric): Implement this task.
-process.exitCode = 0;
+for await (const element of scripts) {
+  try {
+    exitCode = await execute(`pnpm exec ${element}`);
+  } catch (p) {
+    exitCode = p.exitCode;
+  }
+  process.exitCode = exitCode > 0 ? exitCode : 0;
+}
